cf800d538a72eb91fbe8769d8459b3c61cbbcbf4
quiprr <quiprr@ametrine.dev>
2020-10-27 16:12:41 +0000

    v1.0.1

--- src/TwitterNA/TwitterNA.h
+++ src/TwitterNA/TwitterNA.h
@@ -1,18 +1,31 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-@interface TFNTwitterStatus: NSObject
+@interface TFNTwitterStatus : NSObject
 @property(readonly, nonatomic) bool isPromoted;
 @end
 
-@interface TFNDataViewItem: NSObject
+@interface TFNDataViewItem : NSObject
 @property(retain, nonatomic) id _Nullable item;
 @end
 
-@interface TFNDataViewController
-- (TFNDataViewItem *_Nullable)itemsInternalDataViewItemAtValidIndexPath:(id _Nullable)v1;
+@interface TFNItemsDataViewController : NSObject
+@property(copy, nonatomic) NSArray * _Nullable sections;
+- (id _Nullable)itemAtIndexPath:(id _Nullable)arg1;
 @end
 
-@interface TFNItemsDataViewController: TFNDataViewController
-- (id _Nullable)itemAtIndexPath:(id _Nullable)arg1;
+@interface T1URTFooterViewModel : NSObject
+@property(nonatomic, readonly) NSURL * _Nullable url;
+@end
+
+@interface TFNTwitterModuleFooter : NSObject
+@property(nonatomic, readonly) NSURL * _Nullable url;
+@end
+
+@interface T1URTTimelineUserItemViewModel : NSObject
+@property(readonly, nonatomic) NSString * _Nullable scribeComponent;
+@end
+
+@interface TFSTwitterEntityURL : NSObject
+@property(readonly, copy, nonatomic) NSString * _Nullable expandedURL;
 @end
--- src/TwitterNA/TwitterNA.mm
+++ src/TwitterNA/TwitterNA.mm
@@ -1,12 +1,57 @@
 #import "TwitterNA.h"
+// Some of this code is derived from https://github.com/haoict/twitter-no-ads/blob/master/Tweak.xm.
 
 %hook TFNItemsDataViewController
 
-- (id)tableViewCellForItem: (id)v1 atIndexPath: (id)v2
+- (id)tableViewCellForItem:(id)arg1 atIndexPath:(id)arg2
 {
 	UITableViewCell *cell = %orig;
-	id i = [[self itemsInternalDataViewItemAtValidIndexPath: v2] item];
-	if([i respondsToSelector: @selector(isPromoted)] && [i performSelector: @selector(isPromoted)]) [cell setHidden: YES];
+	id item = [self itemAtIndexPath: arg2];
+	NSString *clsNm = NSStringFromClass([item classForCoder]);
+	id i = [self itemAtIndexPath: arg2];
+	if ([i respondsToSelector: @selector(isPromoted)] && [i performSelector: @selector(isPromoted)]) [cell setHidden: YES];
+
+	if ([clsNm isEqualToString:@"TFNTwitterModuleFooter"] && [((TFNTwitterModuleFooter *)item).url.absoluteString containsString:@"connect_people"])
+	{
+		[cell setHidden: YES];
+		return cell;
+	}
+
+	if ([clsNm isEqualToString:@"T1URTFooterViewModel"] && [((T1URTFooterViewModel *)item).url.absoluteString containsString:@"connect_people"] )
+	{
+		[cell setHidden: YES];
+		return cell;
+	}
+
+	if ([clsNm isEqualToString:@"T1Twitter.URTTimelineTrendViewModel"] || [clsNm isEqualToString:@"T1Twitter.URTTimelineEventSummaryViewModel"] || [clsNm isEqualToString:@"T1URTTimelineMessageItemViewModel"])
+	{
+		[cell setHidden: YES];
+		return cell;
+	}
+
+	if ([clsNm isEqualToString:@"TFNTwitterUser"] && [NSStringFromClass([self class]) isEqualToString:@"T1HomeTimelineItemsViewController"])
+	{
+		[cell setHidden: YES];
+		return cell;
+	}
+
+	if ([clsNm isEqualToString:@"T1URTTimelineUserItemViewModel"] && [((T1URTTimelineUserItemViewModel *)item).scribeComponent isEqualToString:@"suggest_who_to_follow"])
+	{
+		[cell setHidden: YES];
+		return cell;
+	}
+
+	if ([clsNm isEqualToString:@"T1Twitter.URTModuleHeaderViewModel"])
+	{
+		[cell setHidden: YES];
+		return cell;
+	}
+
+	if ([clsNm isEqualToString:@"T1Twitter.URTTimelineTrendViewModel"] || [clsNm isEqualToString:@"T1Twitter.URTTimelineEventSummaryViewModel"] || [clsNm isEqualToString:@"T1URTTimelineMessageItemViewModel"])
+	{
+		return 0;
+	}
+
 	return cell;	
 }
 
